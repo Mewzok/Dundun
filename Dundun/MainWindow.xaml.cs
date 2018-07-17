e10cd2e74b454b12458aaf49b863907eb66c5ac6
BuildTools <unconfigured@null.spigotmc.org>
2018-07-17 01:04:40 -0600

    Add project files.

---
+++ Dundun/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Dundun
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow AppWindow;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            AppWindow = this;
+            InitPlayer();
+        }
+
+        public static void InitPlayer()
+        {
+            String pathString = AppDomain.CurrentDomain.BaseDirectory;
+            String pathString2 = System.IO.Path.GetFullPath(System.IO.Path.Combine(pathString, @"..\..\"));
+            Sprites ethan = new Sprites(pathString2 + "Resources\\Sprites\\ethan.png");
+            //+ "Resources\\Sprites\\ethan.png"
+            ethan.SetSprites();
+
+        }
+
+
+
+    }
+}
---
+++ Dundun/Sprites.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Drawing;
+using System.IO;
+
+namespace Dundun
+{
+    class Sprites
+    {
+        private String sheetLoc;
+
+        public Sprites(String sheetLoc)
+        {
+            this.sheetLoc = sheetLoc;
+        }
+
+        public void SetSprites()
+        {
+            List<Bitmap> bmps = new List<Bitmap>(8) {};
+            Rectangle rect = new Rectangle(0, 0, 16, 16);
+            Bitmap src = Image.FromFile(sheetLoc) as Bitmap;
+
+            // Sprites are:
+            // 0 = Standing Forward
+            // 1 = Standing Backward
+            // 2 = Standing Left
+            // 3 = Standing Right
+            // 4 = Walking Forward
+            // 5 = Walking Backward
+            // 6 = Walking Left
+            // 7 = Walking Right
+            for (int i = 0; i <= bmps.Capacity; i++)
+            {
+                MessageBox.Show(bmps.Capacity.ToString());
+                bmps[i] = CropImage(src, rect);
+                rect.X += 16;
+            }
+
+            List<BitmapImage> bmpImg = BitmapToImage(bmps);
+            InitImage(bmpImg);
+        }
+
+        public void InitImage(List<BitmapImage> bmpImg)
+        {
+            MainWindow.AppWindow.player.Source = bmpImg[0];
+        }
+
+        private void Move()
+        {
+
+        }
+
+        private static Bitmap CropImage(Image img, Rectangle cropArea)
+        {
+            Bitmap bmpImage = new Bitmap(img);
+            return bmpImage.Clone(cropArea, bmpImage.PixelFormat);
+        }
+
+        private List<BitmapImage> BitmapToImage(List<Bitmap> bmps)
+        {
+            List<BitmapImage> bmpImg = new List<BitmapImage> { };
+            for (int i = 0; i <= 7; i++)
+            {
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    bmps[i].Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                    memory.Position = 0;
+                    BitmapImage bitmapimage = new BitmapImage();
+                    bitmapimage.BeginInit();
+                    bitmapimage.StreamSource = memory;
+                    bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapimage.EndInit();
+
+                    bmpImg[i] = bitmapimage;
+                }
+            }
+            return bmpImg;
+        }
+    }
+}
